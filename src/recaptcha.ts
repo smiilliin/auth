de6760635a64820aeccb161237f9667c775849c3
smiilliin <smiilliindeveloper@gmail.com>
2023-01-08 22:17:52 +0900

    Removed remarks

--- src/recaptcha.ts
+++ src/recaptcha.ts
@@ -1,14 +1,6 @@
 import https from "https";
 
 const checkRecaptcha = (g_response: string): Promise<boolean> => {
-  //   return new Promise<boolean>((resolve) => {
-  //     const req = https.request("https://www.google.com/recaptcha/api/siteverify");
-  //     req.write({
-  //       secret: process.env["RECAPTCHA_KEY"],
-  //       response: g_response,
-  //     });
-  //     req.end();
-  //   });
   return new Promise<boolean>((resolve) => {
     fetch("https://www.google.com/recaptcha/api/siteverify", {
       method: "POST",
